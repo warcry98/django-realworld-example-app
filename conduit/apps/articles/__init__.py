57eff1659cf7a76795682f08b33fc1519640f0b8
Wahid Sholihul Fikri <wahidsf1905@gmail.com>
2025-05-12 00:46:52 +0700

    Migrate models

---
+++ conduit/apps/articles/__init__.py
@@ -0,0 +1,11 @@
+# from django.apps import AppConfig
+
+# class ArticlesAppConfig(AppConfig):
+#     name = 'conduit.apps.articles'
+#     label = 'articles'
+#     verbose_name = 'Articles'
+
+#     def ready(self):
+#         return super().ready()
+
+# default_app_config = 'conduit.apps.articles.ArticlesAppConfig'
--- conduit/settings.py
+++ conduit/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
+    'django_extensions',
+    'rest_framework',
+
+    'conduit.apps.articles',
+    'conduit.apps.authentication',
+    'conduit.apps.core',
+    'conduit.apps.profiles',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'conduit.urls'
@@ -120,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+AUTH_USER_MODEL = 'authentication.User'
